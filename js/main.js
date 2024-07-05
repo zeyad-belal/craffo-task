23ec9cdce2e90b5d28c3f199f5166d4889669a1f
zeyad belal <zeyadbelal00@gmail.com>
2024-07-05 20:52:59 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-document.getElementById('clickMe').addEventListener('click', () => {
-  alert('Button clicked!');
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    speed: 400,
+    spaceBetween: 100,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      renderCustom: function (swiper, current, total) {
+        return current + ' of ssssssssssssssss' + total;
+      }
+    },
+  });
+
+  const Myswiper = document.querySelector(".swiper").swiper;
+
+  // Now you can use all slider methods like
+  Myswiper.slideNext();
 });
+
+
+
