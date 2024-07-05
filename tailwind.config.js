b50464743d7c1ad1cc2dbc62a156e57c7ec5ec24
zeyad belal <zeyadbelal00@gmail.com>
2024-07-05 14:58:27 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './js/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
